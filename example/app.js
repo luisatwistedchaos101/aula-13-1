e5872d8f38bf05b27f50a44c2542d7d8586521c8
Wallace027dev <wallaceofc@hotmail.com>
2024-08-20 10:43:26 -0300

    exercicio de conta bancaria

--- example/app.js
+++ example/app.js
@@ -4,29 +4,9 @@ class ContaBancaria {
     this.saldo = saldo;
   }
 
-  depositar(valor) {
-    if (valor > 0) {
-      this.saldo += valor;
-      console.log(`Depósito de R$${valor.toFixed(2)} realizado com sucesso.`);
-    } else {
-      console.log("O valor do depósito deve ser um valor positivo.");
-    }
-  }
+  depositar(valor) {}
 
-  retirar(valor) {
-    if (valor > 0) {
-      if (this.saldo >= valor) {
-        this.saldo -= valor;
-        console.log(`Retirada de R$${valor.toFixed(2)} realizada com sucesso.`);
-      } else {
-        console.log('Saldo insuficiente para a retirada.');
-      }
-    } else {
-      console.log('O valor da retirada deve ser positivo.');
-    }
-  }
+  retirar(valor) {}
 
-  consultarSaldo() {
-    console.log(`O saldo atual é: ${this.saldo.toFixed(2)}.`)
-  }
+  consultarSaldo() {}
 }
