0fae15606f69a9facec32ac4b3d416ea5fd110ef
luisatwistedchaos101 <luisaseafox@gmail.com>
2024-08-22 20:18:19 -0300

    atividade feita!

--- example/app.js
+++ example/app.js
@@ -1,12 +1,28 @@
-const ContaBancaria = {
-  constructor(titular, saldo) {
+function ContaBancaria (titular, saldo) {
     this.titular = titular;
     this.saldo = saldo;
+
+  this.depositar = function (valor) {
+    this.saldo += valor
+    console.log(`Saldo de R$${valor} depositado com sucesso.`)
   },
 
-  depositar: function (valor) {},
+  this.retirar = function (valor) {
+    if (this.saldo > valor){
+      this.saldo -= valor
+      console.log(`Saldo de R$${valor} retirado com sucesso.`)
+    } else {
+      console.log (`Saldo é insuficiente.`)
+    }
+  },
 
-  retirar: function (valor) {},
-
-  consultarSaldo: function () {}
+  this.consultarSaldo = function () {
+    console.log (`\nTitular: 4{this.titular}\nSaldo: ${this.saldo}`)
+  }
 };
+
+const luisadeAlmeida = new ContaBancaria("luisadeAlmeida", 20.00);
+
+luisadeAlmeida.consultarSaldo()
+luisadeAlmeida.depositar(10)
+luisadeAlmeida.retirar(5)
